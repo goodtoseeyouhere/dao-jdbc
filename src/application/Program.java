025be45de6c4c75224946101772d21394dbbdf34
goodtoseeyouhere <themaninthewall59@gmail.com>
2024-07-16 19:26:36 -0300

    criado sellerdaoJDBC e DAOfacotry alem da implementacao do metodo findbyid

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,15 @@
 package application;
 
-import java.util.Date;
-
-import models.entities.Department;
+import models.dao.DaoFactory;
+import models.dao.SellerDao;
 import models.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department dp = new Department(1, "books");
-		System.out.println(dp);
-		Seller seller = new Seller(21,"Bernardo", "bernardo@gmail.com", new Date(), 3000.0, dp);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 	}
 
---
+++ src/models/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,84 @@
+package models.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import db.DB;
+import db.DbException;
+import models.dao.SellerDao;
+import models.entities.Seller;
+import models.entities.Department;
+
+public class SellerDaoJDBC implements SellerDao {
+	
+	private Connection conn;
+	
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
+	@Override
+	public void insert(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		try {
+			String query = "SELECT seller.*,department.Name as DepName \r\n"
+					+ "FROM seller INNER JOIN department \r\n"
+					+ "ON seller.DepartmentId = department.Id \r\n"
+					+ "WHERE seller.Id = ?";
+			
+			pst = conn.prepareStatement(query);
+			pst.setInt(1, id);
+			rs = pst.executeQuery();
+			if(rs.next()) {
+				Department dep = new Department();
+				dep.setId(rs.getInt("DepartmentId"));
+				dep.setName(rs.getString("DepName"));
+				Seller obj = new Seller();
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				obj.setEmail(rs.getString("Email"));
+				obj.setBaseSalary(rs.getDouble("BaseSalary"));
+				obj.setBirthDate(rs.getDate("BirthDate"));
+				obj.setDepartment(dep);
+				return obj;				
+			}
+			return null;
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closePreparedStatement(pst);
+			DB.closeResultSet(rs);
+		}
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
